c2acf2f8ddcb08233c7b839a59cf68fa47ecb9c0
naarkz <naarkz@michie.dev>
2022-12-22 22:50:55 +0800

    feat: Add color pallete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-3xl">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl bg-strong-yellow text-dark-blue">
+      <span className="bg-pastel-pink">Hello world!</span>
+    </h1>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
       fontFamily: {
         gtPro: ["GT Eesti Pro Display", "sans-serif"],
       },
+      colors: {
+        "strong-yellow": "#FFFD63",
+        "dark-blue": "#0a0b1e",
+        "dark-gray": "#C7C7CB",
+        "strong-turquoise": "#81f4d6",
+        "pastel-turquoise": "#e6fdf7",
+        "strong-violet" : "#96a7ff",
+        "pastel-violet" : "#eaedff",
+        "strong-orange" : "#ffbf72",
+        "pastel-orange" : "#ffead0",
+        "strong-pink" : "#ff837d",
+        "pastel-pink" : "#ffd6d4"
+      },
     },
   },
   plugins: [],
