d0e71513c16bc6309c2fae8ebfcaf5782049f865
naarkz <naarkz@michie.dev>
2022-12-23 00:22:21 +0800

    feat: Add navigation bar

---
+++ components/navigation/Nav.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Nav() {
+  return (
+    <div className="w-full px-[69px] mt-[50px]">
+      <nav className="w-full h-[64px] pl-[33px] pr-[12px] rounded-[20px] border-solid border-[1px] border-black flex justify-between items-center">
+        <div className="flex">
+          <Image
+            src={"/static/images/logo-small.svg"}
+            alt="naarkz-logo-small"
+            width={33}
+            height={21}
+          />
+          <span className="font-bold text-[20px] ml-[11px]">naark.dev</span>
+        </div>
+        <button
+          type="button"
+          className="font-[400] h-[48px] bg-dark-blue w-[135px] text-white rounded-[12px] flex justify-center items-center"
+        >
+          Contact Me
+        </button>
+      </nav>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import Nav from "../components/navigation/Nav";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl bg-strong-yellow text-dark-blue">
-      <span className="bg-pastel-pink">Hello world!</span>
-    </h1>
+    <>
+      <Nav />
+    </>
   );
 }
