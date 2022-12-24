6a5a5c4f2362fc242fc1b054b91440aafa120d63
naarkz <naarkz@michie.dev>
2022-12-25 04:03:31 +0800

    feat: Add animation to tech stack icons

--- components/layouts/TechStack.tsx
+++ components/layouts/TechStack.tsx
@@ -3,7 +3,7 @@ import TechBox from "../utils/TechBox";
 
 export default function TechStack() {
   return (
-    <div className="mt-[50px]">
+    <div className="my-[50px]">
       <h1 className="font-medium text-[28px]">
         What makes my work possible...
       </h1>
@@ -12,22 +12,35 @@ export default function TechStack() {
         needed. That&apos;s what we developers do :)
       </p>
       <div className="flex flex-wrap gap-[21px] mt-[43px]">
-        <TechBox color="bg-pastel-violet" icon="nextjs.svg" />
+        <TechBox color="bg-pastel-violet" icon="nextjs.svg" name="Next.js" />
         <TechBox color="bg-dark-gray" />
         <TechBox color="bg-dark-gray" />
 
-        <TechBox color="bg-pastel-yellow" icon="react.svg" />
-        <TechBox color="bg-strong-pink" icon="flutter.svg" width={31} />
-        <TechBox color="bg-pastel-blue" icon="shopify.svg" />
+        <TechBox color="bg-pastel-yellow" icon="react.svg" name="React.js" />
+        <TechBox
+          color="bg-strong-pink"
+          icon="flutter.svg"
+          width={31}
+          name="Flutter"
+        />
+        <TechBox color="bg-pastel-blue" icon="shopify.svg" name="Shopify" />
         <TechBox color="bg-dark-gray" />
-        <TechBox color="bg-pastel-orange" icon="redux.svg" />
-        <TechBox color="bg-pastel-turquoise" icon="tailwind.svg" />
-        <TechBox color="bg-strong-violet" icon="firebase.svg" />
+        <TechBox color="bg-pastel-orange" icon="redux.svg" name="Redux" />
+        <TechBox
+          color="bg-pastel-turquoise"
+          icon="tailwind.svg"
+          name="Tailwind"
+        />
+        <TechBox color="bg-strong-violet" icon="firebase.svg" name="Firebase" />
         <TechBox color="bg-dark-gray" />
-        <TechBox color="bg-strong-turquoise" icon="graphql.svg" />
-        <TechBox color="bg-strong-orange" icon="sql.svg" />
+        <TechBox
+          color="bg-strong-turquoise"
+          icon="graphql.svg"
+          name="Graphql"
+        />
+        <TechBox color="bg-strong-orange" icon="sql.svg" name="SQL" />
         <TechBox color="bg-dark-gray" />
-        <TechBox color="bg-pastel-pink" icon="php.svg" />
+        <TechBox color="bg-pastel-pink" icon="php.svg" name="PHP" />
       </div>
     </div>
   );
--- components/utils/TechBox.tsx
+++ components/utils/TechBox.tsx
@@ -5,15 +5,18 @@ export default function TechBox({
   color,
   icon,
   width,
+  name,
 }: {
   color: string;
   icon?: string;
   width?: number;
+  name?: string;
 }) {
   return (
     <div
-      className={`${color} tech-box w-[85px] h-[85px] flex justify-center items-center border-[1px] border-black`}
+      className={`${color} tech-box cursor-pointer w-[85px] h-[85px] flex justify-center items-center border-[1px] border-black`}
     >
+      {name && <span className="tooltip">{name}</span>}
       {icon && (
         <Image
           src={`/static/images/tech_icons/${icon}`}
