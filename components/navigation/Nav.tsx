ce5355e97d9e06399c0cd520a0a481e384fd3498
naarkz <naarkz@michie.dev>
2022-12-24 16:57:48 +0800

    refactor: Show hamburger menu on smaller screens

--- components/navigation/Nav.tsx
+++ components/navigation/Nav.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import useWindowSize from "../../hooks/useWindowSize";
 
 export default function Nav() {
+  const { width } = useWindowSize();
   return (
     <div className="w-full sm:px-[69px] px-[38px] top-[50px] flex justify-center z-[1100] absolute">
       <nav
         id="top-nav"
-        className="w-full h-[64px] bg-white pl-[33px] pr-[12px] rounded-[20px] border-solid border-[1px] border-black flex justify-between items-center"
+        className="w-full h-[64px] bg-white fold:pl-[33px] pl-[12px] pr-[12px] rounded-[20px] border-solid border-[1px] border-black flex justify-between items-center"
       >
         <div className="flex">
           <Image
@@ -17,12 +19,26 @@ export default function Nav() {
           />
           <span className="font-bold text-[20px] ml-[11px]">naark.dev</span>
         </div>
-        <button
-          type="button"
-          className="font-[400] h-[48px] cursor-pointer hover:bg-pastel-blue bg-dark-blue w-[135px] text-white rounded-[12px] flex justify-center items-center"
-        >
-          Contact Me
-        </button>
+        {width > 480 ? (
+          <button
+            type="button"
+            className="font-[400] h-[48px] cursor-pointer hover:bg-pastel-blue bg-dark-blue w-[135px] text-white rounded-[12px] flex justify-center items-center"
+          >
+            Contact Me
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="font-[400] h-[48px] w-[48px] cursor-pointer hover:bg-pastel-blue bg-dark-blue text-white rounded-[12px] flex justify-center items-center"
+          >
+            <Image
+              src={"/static/images/hamburger.svg"}
+              alt="hamburger menu"
+              width={22}
+              height={18}
+            />
+          </button>
+        )}
       </nav>
     </div>
   );
