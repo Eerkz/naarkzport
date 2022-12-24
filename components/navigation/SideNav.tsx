7404f171d35c7410e409e794ce7f5258bfbf1415
naarkz <naarkz@michie.dev>
2022-12-25 04:10:37 +0800

    refactor: Change menu items

--- components/navigation/SideNav.tsx
+++ components/navigation/SideNav.tsx
@@ -4,13 +4,13 @@ export default function SideNav() {
   return (
     <ul className="font-[500] text-[18px] leading-[21px] flex flex-col gap-y-[23px] uppercase pb-[40px]">
       <li className="flex gap-x-[25px]">
-        <span className=" w-[20px] h-auto">01</span>Tech Stack
+        <span className=" w-[20px] h-auto">01</span>Projects
       </li>
       <li className="flex gap-x-[25px]">
-        <span className=" w-[20px] h-auto">02</span>Experience
+        <span className=" w-[20px] h-auto">02</span>Tech Stack
       </li>
       <li className="flex gap-x-[25px]">
-        <span className=" w-[20px] h-auto">03</span>Certifications
+        <span className=" w-[20px] h-auto">03</span>Services
       </li>
       <li className="flex gap-x-[25px]">
         <span className=" w-[20px] h-auto">04</span>Contact Details
