09030bddf18a15fe10d7b6a8c903936356e3341a
naarkz <naarkz@michie.dev>
2022-12-25 20:18:36 +0800

    style: Add social icon animations

--- components/layouts/Contact.tsx
+++ components/layouts/Contact.tsx
@@ -10,54 +10,112 @@ export default function Contact() {
         You can contact me in any of these platform!
       </p>
       <ul
-        className="flex text-[28px] gap-[20px] mt-[20px]"
+        className="flex text-[28px] gap-[20px] mt-[20px] flex-wrap"
         id="social-icons-list"
       >
         <li id="linkedin" className="social-icon ">
-          <a href="https://www.linkedin.com/in/wilma-a-0b6576220/">
-            <Image
-              src={"/static/images/social/linkedin.svg"}
-              alt="linkedin-icon"
-              width={30}
-              height={30}
-            />
-          </a>
+          <div className="social-inner">
+            <div className="social-content social-front">
+              <a
+                href="https://www.linkedin.com/in/wilma-a-0b6576220/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  src={"/static/images/social/linkedin.svg"}
+                  alt="linkedin-icon"
+                  width={30}
+                  height={30}
+                />
+              </a>
+            </div>
+            <div className="social-content social-back">
+              <a
+                href="https://www.linkedin.com/in/wilma-a-0b6576220/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  src={"/static/images/social/link.svg"}
+                  alt="link-icon"
+                  width={25}
+                  height={25}
+                />
+              </a>
+            </div>
+          </div>
         </li>
         <li id="email" className="social-icon ">
-          <a href="#">
-            <CopyToClipboard
-              text={"https://www.linkedin.com/in/wilma-a-0b6576220/"}
-            >
-              <Image
-                src={"/static/images/social/discord.svg"}
-                alt="discord-icon"
-                width={30}
-                height={30}
-              />
-            </CopyToClipboard>
-          </a>
+          <div className="social-inner">
+            <div className="social-content social-front">
+              <CopyToClipboard text={"Naarkz#7945"}>
+                <Image
+                  src={"/static/images/social/discord.svg"}
+                  alt="discord-icon"
+                  width={30}
+                  height={30}
+                />
+              </CopyToClipboard>
+            </div>
+            <div className="social-content social-back">
+              <CopyToClipboard text={"Naarkz#7945"}>
+                <Image
+                  src={"/static/images/social/copy.svg"}
+                  alt="copy-icon"
+                  width={25}
+                  height={25}
+                />
+              </CopyToClipboard>
+            </div>
+          </div>
         </li>
         <li id="discord" className="social-icon ">
-          <a href="#">
-            <CopyToClipboard text={"Naark#7945"}>
-              <Image
-                src={"/static/images/social/email.svg"}
-                alt="email-icon"
-                width={30}
-                height={30}
-              />
-            </CopyToClipboard>
-          </a>
+          <div className="social-inner">
+            <div className="social-content social-front">
+              <CopyToClipboard text={"wcalemios@gmail.com"}>
+                <Image
+                  src={"/static/images/social/email.svg"}
+                  alt="email-icon"
+                  width={30}
+                  height={30}
+                />
+              </CopyToClipboard>
+            </div>
+            <div className="social-content social-back">
+              <CopyToClipboard text={"wcalemios@gmail.com"}>
+                <Image
+                  src={"/static/images/social/copy.svg"}
+                  alt="copy-icon"
+                  width={25}
+                  height={25}
+                />
+              </CopyToClipboard>
+            </div>
+          </div>
         </li>
         <li id="whatsapp" className="social-icon ">
-          <a href="#">
-            <Image
-              src={"/static/images/social/whatsapp.svg"}
-              alt="whatsapp-icon"
-              width={30}
-              height={30}
-            />
-          </a>
+          <div className="social-inner">
+            <div className="social-content social-front">
+              <CopyToClipboard text={"https://wa.me/63948989664"}>
+                <Image
+                  src={"/static/images/social/whatsapp.svg"}
+                  alt="whatsapp-icon"
+                  width={30}
+                  height={30}
+                />
+              </CopyToClipboard>
+            </div>
+            <div className="social-content social-back">
+              <CopyToClipboard text={"https://wa.me/63948989664"}>
+                <Image
+                  src={"/static/images/social/copy.svg"}
+                  alt="copy-icon"
+                  width={25}
+                  height={25}
+                />
+              </CopyToClipboard>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
